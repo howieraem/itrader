e62cea0a162e90910087336fb2567950a99e2336
howieraem <34226262+howieraem@users.noreply.github.com>
2021-04-07 09:32:05 +0800

    change to typescript

--- ui/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-// 跨域请求
-const {createProxyMiddleware} = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    app.use(createProxyMiddleware('/listUserByName', {target: 'http://localhost:8092/user/', changeOrigin: true}))
-}
