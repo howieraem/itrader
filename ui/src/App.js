4c6c4341e4775751c3ac8294bb452299cc2eb782
howieraem <34226262+howieraem@users.noreply.github.com>
2021-04-16 13:13:34 +0800

    added appbar

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
+import PrimarySearchAppBar from './components/bar/AppBar';
 
 function App() {
   let date = new Date();
@@ -7,8 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <PrimarySearchAppBar/>
         <h1>ITrader UI</h1>
         <p>Work in progress...</p>
+        {/* <Button variant="contained">Push!</Button> */}
         <p>{'UTC+' + (0 - date.getTimezoneOffset() / 60)} {date.toLocaleTimeString()}</p>
       </header>
     </div>
--- ui/src/index.js
+++ ui/src/index.js
@@ -13,7 +13,7 @@ function main() {
     );
 }
 
-setInterval(main, 1000)
+setInterval(main, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
