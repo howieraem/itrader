29835809a1cfa5b83996e43eee7c3bb6bf6dc7ce
howieraem <34226262+howieraem@users.noreply.github.com>
2021-08-12 16:42:01 +0800

    build front end with mvn

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, parallel } = require('gulp');
+const del = require('del');
+const log = require('fancy-log');
+
+const paths = {
+  react_src: 'ui/build/**/*',
+  react_dist: 'src/main/resources/static/'
+};
+
+function clean()  {
+  log('removing the old files in the directory')
+  return del('src/main/resources/static/**', {force:true});
+}
+
+function copyReactCodeTask() {
+  log('copying React code into the directory')
+  return src(`${paths.react_src}`)
+    .pipe(dest(`${paths.react_dist}`));
+}
+
+clean();
+copyReactCodeTask();
